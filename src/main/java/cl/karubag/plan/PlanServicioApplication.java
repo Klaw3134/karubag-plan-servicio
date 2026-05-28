768f08a7f25a86352e59ff84534e7015581aee67
klaw3134 <cgnavarrete3134@gmail.com>
2026-05-28 11:35:37 -0400

    init: plan-servicio

---
+++ src/main/java/cl/karubag/plan/PlanServicioApplication.java
@@ -0,0 +1,13 @@
+package cl.karubag.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanServicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/karubag/plan/PlanServicioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.karubag.plan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanServicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
